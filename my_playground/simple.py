6e841914e06c46bc135c9268146541b7dfd2ae3d
frank-Regal <fregal@utexas.edu>
2024-03-14 15:23:16 -0500

    first push

---
+++ my_playground/simple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Recognize a single utterance from a WAV file.
+
+Supporting other file types is left as an exercise to the reader.
+"""
+
+# MIT license (c) 2022, see LICENSE for more information.
+# Author: David Huggins-Daines <dhdaines@gmail.com>
+
+from pocketsphinx import Decoder
+import argparse
+import wave
+
+# may need to convert wave file to 16-bit PCM
+# 'ffmpeg -i recorded_audio.wav -acodec pcm_s16le new_recorded_audio.wav'
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("audio", help="Audio file to recognize")
+args = parser.parse_args()
+with wave.open(args.audio, "rb") as audio:
+    decoder = Decoder(samprate=audio.getframerate())
+    decoder.start_utt()
+    decoder.process_raw(audio.getfp().read(), full_utt=True)
+    decoder.end_utt()
+    print(decoder.hyp().hypstr)
